60aec78e407b1735970fc4839d6addd03d0de0b3
Cony <Cony@Cony-PC>
2013-11-26 03:13:40 +0800

    merge

--- src/main/java/com/papteco/web/controllers/UsersController.java
+++ src/main/java/com/papteco/web/controllers/UsersController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
 import com.papteco.web.beans.RoleBean;
 import com.papteco.web.beans.UsersBean;
 import com.papteco.web.beans.UsersFormBean;
@@ -25,77 +24,76 @@ import com.papteco.web.utils.WebUtils;
 public class UsersController extends BaseController {
 	@Autowired
 	private UserServiceImpl userService;
-
+	
 	@RequestMapping(method = RequestMethod.GET, value = "doSearchUser")
 	@ResponseBody
 	public List doSearchUser(@RequestParam String searchRoles,
 			@RequestParam String searchUserName) throws Exception {
-		System.out.println("searchRoles:" + searchRoles + " searchUserName:"
-				+ searchUserName);
+		System.out.println("searchRoles:"+searchRoles+" searchUserName:"+searchUserName);
 		return WebUtils.toSearchUserGrid(searchRoles, searchUserName);
 	}
-
+	
+	
 	@RequestMapping(method = RequestMethod.GET, value = "loadingRoles")
 	@ResponseBody
 	public Map loadingRoles() {
 		return WebUtils.toRolesJson(userrolesSetting.getAllRoles());
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST, value = "createUserRequest")
 	@ResponseBody
 	public Map createUserRequest(@RequestBody UsersFormBean bean)
 			throws Exception {
-
-		System.out.println("createUserRequest UsersFormBean:" + bean);
+		
+		System.out.println("createUserRequest UsersFormBean:"+bean);
 		UsersBean user = preUserBean(bean, userrolesSetting.getAllRoles());
-
-		try {
-			if (userService.getUser(bean.getCreateUserName()) != null) {
-				return ImmutableMap.of("type", "fail", "message",
-						"User already exists");
-			} else {
+		
+		try{
+			if(userService.getUser(bean.getCreateUserName())!=null){
+				return ImmutableMap.of("type", "fail","message","User already exists");
+			}else{
 				userService.saveUser(user);
 				return ImmutableMap.of("type", "success");
 			}
-
-		} catch (Exception e) {
+			
+		}catch(Exception e){
 			e.printStackTrace();
-			return ImmutableMap.of("type", "fail", "message", e.getMessage());
-
+			return ImmutableMap.of("type", "fail","message",e.getMessage());
+			
 		}
-
+		
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST, value = "updateUserRequest")
 	@ResponseBody
 	public Map updateUserRequest(@RequestBody UsersFormBean bean)
 			throws Exception {
-
-		System.out.println("updateUserRequest UsersFormBean:" + bean);
-
-		try {
+		
+		System.out.println("updateUserRequest UsersFormBean:"+bean);
+		
+		
+		try{
 			UsersBean user = preUserBean(bean, userrolesSetting.getAllRoles());
-
+			
 			userService.saveUser(user);
 			return ImmutableMap.of("type", "success");
-		} catch (Exception e) {
+		}catch(Exception e){
 			e.printStackTrace();
-			return ImmutableMap.of("type", "fail", "message", e.getMessage());
-
+			return ImmutableMap.of("type", "fail","message",e.getMessage());
+			
 		}
 	}
-
-	private UsersBean preUserBean(UsersFormBean bean,
-			Map<String, String> allroles) {
+	
+	private UsersBean preUserBean(UsersFormBean bean, Map<String, String> allroles){
 		UsersBean user = new UsersBean();
-		if (StringUtils.isNotBlank(bean.getCreateUserName()))
+		if(StringUtils.isNotBlank(bean.getCreateUserName()))
 			user.setUserName(bean.getCreateUserName());
-		if (StringUtils.isNotBlank(bean.getCreatePassword()))
+		if(StringUtils.isNotBlank(bean.getCreatePassword()))
 			user.setPassword(bean.getCreatePassword());
 		user.setEmail(bean.getCreateEmail());
-
+		
 		List<RoleBean> roles = new ArrayList<RoleBean>();
-		for (String role : bean.getCreateRoles()) {
+		for(String role : bean.getCreateRoles()){
 			roles.add(new RoleBean(role, allroles.get(role)));
 		}
 		user.setRoles(roles);
@@ -124,37 +122,61 @@ public class UsersController extends BaseController {
 	@ResponseBody
 	public Map getUsersRoleList(@RequestBody UsersFormBean userid)
 			throws Exception {
+		
+		System.out.println("getUsersRoleList() userid:"+userid.getCreateUserName());
 
-		System.out.println("getUsersRoleList() userid:"
-				+ userid.getCreateUserName());
-
-		if (StringUtils.isBlank(userid.getCreateUserName())) {
-			return ImmutableMap.of("type", "success", "username", "", "email",
-					"", "roles", this.populateUserRolsesInputs(userrolesSetting
-							.getAllRoles()));
-		} else {
-
+		if(StringUtils.isBlank(userid.getCreateUserName())){
+			return ImmutableMap.of("type", "success",
+					"username","",
+					"password","",
+					"email","",
+					"roles",this.populateUserRolsesCreate(userrolesSetting.getAllRoles()));
+		}else{
 			UsersBean user = userService.getUser(userid.getCreateUserName());
-			if (user != null) {
-				return ImmutableMap.of("type", "success", "username", user
-						.getUserName(), "email", user.getEmail(), "roles", this
-						.populateUserRolsesInputs(userrolesSetting
-								.getAllRoles()));
-			} else {
-				return ImmutableMap.of("type", "success", "username",
-						"username1", "email", "email1", "roles", this
-								.populateUserRolsesInputs(userrolesSetting
-										.getAllRoles()));
+			if(user != null){
+				return ImmutableMap.of("type", "success",
+						"username",user.getUserName(),
+						"password",user.getPassword(),
+						"email",user.getEmail(),
+						"roles",this.populateUserRolsesModify(userrolesSetting.getAllRoles(), user));	
+			}else{
+				return ImmutableMap.of("type", "success",
+						"username","username1",
+						"password","",
+						"email","email1",
+						"roles",this.populateUserRolsesCreate(userrolesSetting.getAllRoles()));	
 			}
 		}
 	}
-
-	private String populateUserRolsesInputs(Map<String, String> userroles) {
+	
+	private String populateUserRolsesCreate(Map<String, String> userroles){
 		StringBuffer sb = new StringBuffer();
-		if (userroles != null && userroles.size() != 0) {
+		if(userroles != null && userroles.size() != 0){
 			sb.append("<table border='0' cellspacing='0' cellpadding='0'>");
-			for (String key : userroles.keySet()) {
+			for(String key : userroles.keySet()){
 				sb.append("<tr><td><input type='checkbox' class='rolecls' name='roles' value ='");
+				sb.append(key);
+				sb.append("' ></td><td>");
+				sb.append(userroles.get(key));
+				sb.append("</td></tr>");
+			}
+			sb.append("</table>");
+		}
+		return sb.toString();
+	}
+	
+	private String populateUserRolsesModify(Map<String, String> userroles, UsersBean user){
+		StringBuffer sb = new StringBuffer();
+		List<RoleBean> roles = user.getRoles();
+		if(userroles != null && userroles.size() != 0){
+			sb.append("<table border='0' cellspacing='0' cellpadding='0'>");
+			for(String key : userroles.keySet()){
+				sb.append("<tr><td><input type='checkbox' class='rolecls' name='roles'");
+				for(RoleBean role : roles){
+					if(role.getRoleCde().equals(key))
+						sb.append(" checked = true");
+				}
+				sb.append(" value ='");
 				sb.append(key);
 				sb.append("' ></td><td>");
 				sb.append(userroles.get(key));
